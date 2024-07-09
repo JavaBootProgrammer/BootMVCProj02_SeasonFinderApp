6d1b0d5d5399b125fea533a3b58b41662a4702fe
anand8533 <161494555+anand8533@users.noreply.github.com>
2024-07-09 15:05:04 -0400

    2

--- src/main/java/com/nt/controller/SeasonFinderController.java
+++ src/main/java/com/nt/controller/SeasonFinderController.java
@@ -5,6 +5,7 @@ import com.nt.service.SeasonFinderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
@@ -14,17 +15,18 @@ public class SeasonFinderController {
     @Autowired
     private SeasonFinderService seasonFinderService;
 
-    @GetMapping("/welcome")
+    @RequestMapping("/welcome")
     public String homePage() {
-        System.out.println("home page");
+        System.out.println("home page displayed");
 
         return "welcome";
     }
 
-    @GetMapping("/season")
-    public String homePage(Map<String, Object> map) {
+    @RequestMapping("/season")
+    public String displaySeason(Map<String, Object> map) {
         String season = seasonFinderService.findSeason();
         map.put("season", season);
+        System.out.println("displaySeason page displayed");
         return "displaySeason";
     }
 }
